6414f16faff14abd8c8cb67db826dc24a750f48f
Your Name <danykaram97@gmail.com>
2020-06-12 18:19:56 -0400

    Again

--- src/scripts/Main.gd
+++ src/scripts/Main.gd
@@ -22,6 +22,7 @@ func _process(delta):
 
 
 func _calcular_timer():
+	"""
 	time_now = OS.get_datetime()
 	limit = time_now
 	$Timer.wait_time = 0
@@ -43,6 +44,7 @@ func _calcular_timer():
 			$Timer.wait_time = float(limit['second'])
 			$Timer.start()
 			print('HOla bb')
+			"""
 
 
 func _on_Exit_pressed():
