387aa3fb185b1f28ccf1a2d5346b3c186ae44795
Your Name <danykaram97@gmail.com>
2020-06-12 17:20:59 -0400

    Doing some changes. Trying to implement the translation part.

--- src/scripts/ThrowIn.gd
+++ src/scripts/ThrowIn.gd
@@ -9,13 +9,14 @@ extends Node2D
 var time = null
 var score = null
 var cards = null
-
+var user_ans=-1
 var numero_preguntas_nivel = 10
 var turn
 #var user_ans 
 
 # Cargamos nuestro fichero de preguntas en una variable.
 onready var preguntas_del_nivel:Dictionary  = Game.load_data(Game.questions)
+onready var promt_tween=$Tween
 
 var correct_answer
 
@@ -41,7 +42,7 @@ func inicializar():
 #	randomize() # Actualiza la semilla para generar aleatorios.
 #	indices.shuffle() # Barajea aleatoriamente el array de indices.
    
-#	conectar_botones()
+	#conectar_botones()
 #	option_pressed()
 	carga_pregunta()
 
@@ -93,6 +94,7 @@ func _on_Btn_Question_pressed():
 	$Question.visible = true
 	$Btn_Back.visible = true
 	$Footer.visible = false
+	$Before_Answer.visible = false
 
 
 func _on_Btn_home_pressed():
@@ -100,33 +102,48 @@ func _on_Btn_home_pressed():
 	pass
 	
 func _on_Option_1_pressed():
-	_prompt_if_pressed($Question/Options.get_child(0).get_meta("answer"))
+	#$Before_Answer.visible = true
+	user_ans = $Question/Options.get_child(0).get_meta("answer")
+	_prompt_and_check()
 	pass # Replace with function body.
 
 func _on_Option_2_pressed():
-	_prompt_if_pressed($Question/Options.get_child(1).get_meta("answer"))
+	#$Before_Answer.visible = true
+	user_ans = $Question/Options.get_child(1).get_meta("answer")
+	_prompt_and_check()
 	pass # Replace with function body.
 
 func _on_Option_3_pressed():
-	_prompt_if_pressed($Question/Options.get_child(2).get_meta("answer"))
+	#$Before_Answer.visible = true
+	user_ans = $Question/Options.get_child(2).get_meta("answer")
+	_prompt_and_check()
 	pass # Replace with function body.
 	
 func _on_Option_4_pressed():
-	_prompt_if_pressed($Question/Options.get_child(3).get_meta("answer"))
+	#$Before_Answer.visible = true
+	user_ans = $Question/Options.get_child(3).get_meta("answer")
+	_prompt_and_check()
 	
+func _prompt_and_check():
+	$Before_Answer.visible = true
 
-func _prompt_if_pressed(user_ans):
-
-	$Answer_Window.visible = true
+func _on_Btn_Yes_pressed():
+	$Before_Answer.visible = false
+	#$Answer_Window.visible = true
 	$Question.visible = false
 	$Btn_Back.visible = true
 
+	print('it workssss I guess')
 	if(user_ans==correct_answer):
+		$Before_Answer.visible = false
+		$Answer_Window.visible = true
 		_aumentar_score()
 		print('it works')
 		get_node("Answer_Window/proceed_label").text="¡Respuesta Correcta!."
 		#get_tree().reload_current_scene()
 	else:
+		$Before_Answer.visible = false
+		$Answer_Window.visible = true
 		GlobalVar._aumentar_tarjeta()
 		get_node("Answer_Window/proceed_label").text="¡Respuesta Incorrecta!."
 		print('doesnt work')
@@ -138,7 +155,58 @@ func _prompt_if_pressed(user_ans):
 		carga_pregunta()
 	else:
 		print('Game Over')
+	
+	
+func _on_Btn_No_pressed():
+	$Before_Answer.visible = false
+	$Answer_Window.visible = false
+	$Question.visible = true
+	pass # Replace with function body.
+	
+	"""
+func al_responder(boton):
+	var test = false
+   
+	# comprobamos si es el boton correcto
+	if boton.text == 'Yes':
+		test = true
+	else:
+	   print('no se')
+	return test
+	"""
 
+	"""
+func _prompt_if_pressed(user_ans):
+
+	$Before_Answer.visible = true
+	#$Answer_Window.visible = true
+	$Question.visible = false
+	$Btn_Back.visible = true
+
+	if(al_responder(boton)):
+
+		print('it workssss I guess')
+		if(user_ans==correct_answer):
+			_aumentar_score()
+			print('it works')
+			get_node("Answer_Window/proceed_label").text="¡Respuesta Correcta!."
+			#get_tree().reload_current_scene()
+		else:
+			GlobalVar._aumentar_tarjeta()
+			get_node("Answer_Window/proceed_label").text="¡Respuesta Incorrecta!."
+			print('doesnt work')
+	 
+		# comprobamos si es el final del juego
+		if turn < numero_preguntas_nivel-1:
+			turn += 1 # pasamos a otra pregunta
+			get_tree().change_scene("")
+			carga_pregunta()
+		else:
+			print('Game Over')
+	else:
+		print('FF')
+		"""
+		
 func _on_Btn_Back_pressed():
 	$Background.visible = false
 	$Players.visible = true
@@ -164,6 +232,7 @@ func carga_pregunta():
 	botones_random[0].get_node("lbl").text = pregunta_actual["respA"]
 	#botones_random[0] = int($Question/Options/Option_1/lbl_pos.text)
 	botones_random[0].set_meta("answer", 0)
+	#botones_random[0].get_node("lbl").text = pregunta_actual["respA"]
 	print(botones_random[0])
 	botones_random[1].get_node("lbl").text = pregunta_actual["respB"]
 #	botones_random[1] = int($Question/Options/Option_2/lbl_pos.text)
@@ -185,25 +254,19 @@ func carga_pregunta():
 #	$Question/Options/Option_4/lbl.text = pregunta_actual["respD"]
 	
 	correct_answer = pregunta_actual["buena"]
+	#translation_c_answer = pregunta_actual["translationA"]
+	$Before_Answer/translation.text = pregunta_actual["translationA"]
+	"""
+	if(botones_random[0]):
+		$Before_Answer/translation.text = pregunta_actual["translationA"]
+	elif(botones_random[1]):
+		$Before_Answer/translation.text = pregunta_actual["translationB"]
+	elif(botones_random[2]):
+		$Before_Answer/translation.text = pregunta_actual["translationC"]
+	elif(botones_random[3]):
+		$Before_Answer/translation.text = pregunta_actual["translationD"]
+		"""
 
-#func option_pressed():
-#	var a
-#	a =  int($Question/Options/Option_1/lbl_pos.text)
-#	print(a)
-#	if(a == 1):
-#		print('works')
-#	else:
-#		print('doesnt work')
-#	if(botones_random[0]):
-#		user_ans = 0
-#	elif(botones_random[1]):
-#		user_ans = 1
-#	elif(botones_random[2]):
-#		user_ans = 2
-#	elif(botones_random[3]):
-#		user_ans = 3
-#		print(user_ans)
-#	return user_ans		
 
 func elige_pregunta(id_pregunta:String):
 	# Comprobamos si el índice existe.
@@ -225,6 +288,14 @@ func contar_indices():
 #		if button is TextureButton: # Comprueba si cada hijo es del tipo Button
 #			# conectamos la señal PRESSED con la funcion AL_RESPONDER y pasamos el boton como parametro
 #			button.connect("pressed",self,"_prompt_if_pressed")
+"""
+func conectar_botones():
+	var botones = $Before_Answer/Opt.get_children() # Captura los hijos del nodo respuestas
+	for boton in botones:
+		if boton is Button: # Comprueba si cada hijo es del tipo Button
+			# conectamos la señal PRESSED con la funcion AL_RESPONDER y pasamos el boton como parametro
+			boton.connect("pressed",self,"al_responder",[boton])
+			"""
 			
 func _on_Btn_Next_pressed():
 	pass # Replace with function body.
@@ -232,5 +303,3 @@ func _on_Btn_Next_pressed():
 func salir():
 	get_tree().change_scene("res://src/scenes/Main.tscn")
 
-
-	
